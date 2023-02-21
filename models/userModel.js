e6bd0602a5ad875ef9cf15342eba1f872e113d8b
Jaguso <jagutzo@gmail.com>
2023-02-21 17:26:43 -0600

    Added address data

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,25 @@
 const mongoose = require('mongoose');
 
+const addressSchema = new mongoose.Schema({
+    street: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    postalcode: {
+        type: String,
+        required: true
+    }
+});
+
+
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -8,9 +28,21 @@ const userSchema = new mongoose.Schema({
     lastName: {
         type: String,
         required: [true, 'A las name must be provided']
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    birthDate: {
+        type: String,
+        required: true
+    },
+    address: { 
+        type: addressSchema, 
+        required: true 
     }
 });
 
-const User = mongoose.model('User', userSchema);
 
+const User = mongoose.model('User', userSchema);
 module.exports = User;
